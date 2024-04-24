ef94732f63e8200e0522bb9121be0defbda5c593
Anmol Gupta <anmolgupta520@gmail.com>
2024-04-24 22:36:03 +0530

    parse integrers

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"monkey-lang/ast"
 	"monkey-lang/lexer"
 	"monkey-lang/token"
+	"strconv"
 )
 
 const (
@@ -43,6 +44,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// Reading two token so currToken and peekToken are set
 	p.nextToken()
@@ -142,6 +144,23 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 	return leftExp
 
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{
+		Token: p.curToken,
+	}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
 
 func (p *Parser) parseIdentifier() ast.Expression {
